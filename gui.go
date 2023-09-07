6616ce73dbf6b9d81ba6de027a0616e2dc29b049
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-09-07 22:49:10 +0900

    doesnt work but need to commit

--- gui.go
+++ gui.go
@@ -9,7 +9,7 @@ import (
 
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("main", 0, 0, maxX - 1, maxY - 1); err != nil {
+	if v, err := g.SetView("main", 0, 0, maxX-1, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -18,9 +18,14 @@ func layout(g *gocui.Gui) error {
 			return err
 		}
 		for _, task := range tasks {
-			fmt.Fprintf(v, "[ ] %s\n", task.Name)
+			var mark string
+			if task.IsComplete {
+				mark = "[x] %s\n"
+			} else {
+				mark = "[x] %s\n"
+			}
+			fmt.Fprintf(v, mark, task.Name)
 		}
-		v.Editable = true
 		v.Wrap = true
 		if _, err := g.SetCurrentView("main"); err != nil {
 			return err
@@ -29,8 +34,40 @@ func layout(g *gocui.Gui) error {
 	return nil
 }
 
+func taskIsComplete(task string) bool {
+	return rune(task[1]) == 'x'
+}
+
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
+}
+
+func moveDown(g *gocui.Gui, v *gocui.View) error {
+	v.MoveCursor(0, 1, false)
+	return nil
+}
+
+func moveUp(g *gocui.Gui, v *gocui.View) error {
+	v.MoveCursor(0, -1, false)
+	return nil
+}
+
+func toggleTask(g *gocui.Gui, v *gocui.View) error {
+	x, y := v.Cursor()
+	task, err := v.Line(y)
+	if err != nil {
+		return err
+	}
+	isComplete := taskIsComplete(task)
+	v.SetCursor(1, y)
+	v.EditDelete(false)
+	if isComplete {
+		v.EditWrite(' ')
+	} else {
+		v.EditWrite('x')
+	}
+	v.SetCursor(x, y)
+	return nil
 }
 
 func GuiInit() {
@@ -40,9 +77,21 @@ func GuiInit() {
 	}
 	defer g.Close()
 
+	g.Cursor = true
+
 	g.SetManagerFunc(layout)
 
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", 'k', gocui.ModNone, moveUp); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", 'j', gocui.ModNone, moveDown); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", gocui.KeyEnter, gocui.ModNone, toggleTask); err != nil {
 		log.Panicln(err)
 	}
 
